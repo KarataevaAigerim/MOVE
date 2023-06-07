aff35f343e9209455664084afbe0f816eb1ebec5
aigerimkarataeva <ai.gerim@hotmail.com>
2023-06-07 19:00:04 +0800

    fixed edit page and added style to dashboard page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  attr_accessor :first_name, :last_name, :age, :weight, :height, :gender, :goal, :experience
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :programs
